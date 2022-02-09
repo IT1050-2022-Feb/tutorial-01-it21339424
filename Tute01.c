ff291ad1fd0bea7447bcd48c251bdda532a1aeeb
Thusara <99307800+it21339424@users.noreply.github.com>
2022-02-09 06:22:11 +0000

    Completed 1

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,23 @@
 
 int main() {
   
-  return 0;
+  //variable declaration
+  float x , y ;
+  float avg ;
+
+  //get input from the user
+  printf( "Enter subject 1 marks : " ) ;
+  scanf( "%f" , &x ) ;
+
+  printf( "Enter subject 2 marks : " ) ;
+  scanf( "%f" , &y ) ;
+
+  //Calculating Average
+  avg = (x + y) / 2 ;
+
+  //print Average
+  printf( "Average of the two marks : %.2f" , avg ) ;
+
+  return 0 ;
 }
 
